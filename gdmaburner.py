5b908ca1611f19fda1fc5156ad3583d381e24182
Gabriel Dantas <63476124+gdma2004@users.noreply.github.com>
2021-11-24 17:04:11 -0300

    Update gdmaburner.py

--- gdmaburner.py
+++ gdmaburner.py
@@ -1,36 +1,33 @@
 import os
 from os import path
 
-print(' ---- YOUR PENDRIVE MUST BE FORMATTED ----')
-print(' ')
-print('Specify the path of your .iso file:')
-print(' ')
-pathisoinput = str(input('-> '))
+tamanho = os.get_terminal_size().columns
+
+print('YOUR PENDRIVE MUST BE FORMATTED'.center(tamanho, '-'))
+
+tree = str(input('\nShow your home files? (y/n)\n-> '))
+
+if tree == 'y':
+    os.system('tree ~/')
+else:
+    pass
+
+pathisoinput = str(input('\nSpecify the path of your .iso file:\n-> '))
 parameter_iso = bool(path.exists(pathisoinput))
 
 if parameter_iso == True:
-    print(' ')
-    print('Directory found, proceeding.')
-    print(' ')
-    print('These are your disks and removable devices: ')
-    print(' ')
+    print('\nDirectory found, proceeding.\n\nThese are your disks and removable devices:\n')
+
     os.system('lsblk')
-    print(' ')
-    print('Specify your pendrive:')
-    print(' ')
-    pathpendriveinput = str(input('-> '))
+    pathpendriveinput = str(input('\nSpecify your pendrive:\n-> '))
     pathpendrivecompleto = ('/dev/{}'.format(pathpendriveinput))
     parameter_pendrive = bool(path.exists(pathpendrivecompleto))
     
     if parameter_pendrive == True:
-        print(' ')
-        print('Burning...')
-        print(' ')
+        print('\nBurning...\n')
         os.system('sudo dd if={} of={} bs=4M'.format(pathisoinput, pathpendrivecompleto))
     else:
-        print(' ')
-        print('Pendrive not found.')
+        print('\nPendrive not found.')
 
 else:
-    print(' ')
-    print('Diretory not found.')
+    print('\nDiretory not found.')
